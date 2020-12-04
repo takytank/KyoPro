f5b0b1ee19ce3bba4b70213511ce06ebb046930f
takytank <taki_taki_taky@hotmail.co.jp>
2020-12-04 19:25:52 +0900

    AtCoderプロジェクトの準備

---
+++ projects/AtCoder.Temp/ContestPool.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TakyTank.KyoProLib.CSharp;
+using TakyTank.KyoProLib.CSharp.Core31;
+
+namespace AtCoder.Temp
+{
+	class ContestPool
+	{
+		//const long MODP = 1000000007;
+		//const long INF = 1L << 60;
+
+		public static void A()
+		{
+			using var cin = new Scanner();
+		}
+
+		public static void B()
+		{
+			using var cin = new Scanner();
+		}
+
+		public static void C()
+		{
+			using var cin = new Scanner();
+		}
+
+		public static void D()
+		{
+			using var cin = new Scanner();
+		}
+
+		public static void E()
+		{
+			using var cin = new Scanner();
+		}
+
+		public static void F()
+		{
+			using var cin = new Scanner();
+		}
+
+		public static void G()
+		{
+			using var cin = new Scanner();
+		}
+
+		public static void H()
+		{
+			using var cin = new Scanner();
+		}
+
+		public static void I()
+		{
+			using var cin = new Scanner();
+		}
+
+		public static void J()
+		{
+			using var cin = new Scanner();
+		}
+
+		public static void K()
+		{
+			using var cin = new Scanner();
+		}
+
+		public static void L()
+		{
+			using var cin = new Scanner();
+		}
+
+		public static void M()
+		{
+			using var cin = new Scanner();
+		}
+
+		public static void N()
+		{
+			using var cin = new Scanner();
+		}
+
+		public static void O()
+		{
+			using var cin = new Scanner();
+		}
+
+		public static void P()
+		{
+			using var cin = new Scanner();
+		}
+
+		public static void Q()
+		{
+			using var cin = new Scanner();
+		}
+	}
+}
