e8b7c846a3f807aa0f1543fa8b4bb6c9122f712f
takytank <taki_taki_taky@hotmail.co.jp>
2020-12-12 21:39:10 +0900

    AOJのプロジェクトをC#4指定。

--- projects/AOJ/Program.cs
+++ projects/AOJ/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 
 namespace AOJ
@@ -11,10 +12,140 @@ namespace AOJ
 		static void Main()
 		{
 			var cin = new Scanner2();
-
 #if DEBUG
 			Console.ReadLine();
 #endif
+		}
+	}
+
+	public struct ModInt
+	{
+		//public const long P = 1000000007;
+		public const long P = 998244353;
+		public const long ROOT = 3;
+
+		// (924844033, 5)
+		// (998244353, 3)
+		// (1012924417, 5)
+		// (167772161, 3)
+		// (469762049, 3)
+		// (1224736769, 3)
+
+		private long value_;
+
+		public ModInt(long value) {
+			value_ = value;
+		}
+
+		public ModInt(long value, bool mods)
+		{
+			if (mods) {
+				value %= P;
+				if (value < 0) {
+					value += P;
+				}
+			}
+
+			value_ = value;
+		}
+
+		public static ModInt operator +(ModInt lhs, ModInt rhs)
+		{
+			lhs.value_ = (lhs.value_ + rhs.value_) % P;
+			return lhs;
+		}
+		public static ModInt operator +(long lhs, ModInt rhs)
+		{
+			rhs.value_ = (lhs + rhs.value_) % P;
+			return rhs;
+		}
+		public static ModInt operator +(ModInt lhs, long rhs)
+		{
+			lhs.value_ = (lhs.value_ + rhs) % P;
+			return lhs;
+		}
+
+		public static ModInt operator -(ModInt lhs, ModInt rhs)
+		{
+			lhs.value_ = (P + lhs.value_ - rhs.value_) % P;
+			return lhs;
+		}
+		public static ModInt operator -(long lhs, ModInt rhs)
+		{
+			rhs.value_ = (P + lhs - rhs.value_) % P;
+			return rhs;
+		}
+		public static ModInt operator -(ModInt lhs, long rhs)
+		{
+			lhs.value_ = (P + lhs.value_ - rhs) % P;
+			return lhs;
+		}
+
+		public static ModInt operator *(ModInt lhs, ModInt rhs)
+		{
+			lhs.value_ = lhs.value_ * rhs.value_ % P;
+			return lhs;
+		}
+		public static ModInt operator *(long lhs, ModInt rhs)
+		{
+			rhs.value_ = lhs * rhs.value_ % P;
+			return rhs;
+		}
+		public static ModInt operator *(ModInt lhs, long rhs)
+		{
+			lhs.value_ = lhs.value_ * rhs % P;
+			return lhs;
+		}
+
+		public static ModInt operator /(ModInt lhs, ModInt rhs)
+		{
+			long exp = P - 2;
+			while (exp > 0) {
+				if (exp % 2 > 0) {
+					lhs *= rhs;
+				}
+
+				rhs *= rhs;
+				exp /= 2;
+			}
+
+			return lhs;
+		}
+
+		public static implicit operator ModInt(long n)
+		{
+			return new ModInt(n, true);
+		}
+
+		public static ModInt Inverse(ModInt value)
+		{
+			return Pow(value, P - 2);
+		}
+
+		public static ModInt Pow(ModInt value, long k)
+		{
+			return Pow(value.value_, k);
+		}
+
+		public static ModInt Pow(long value, long k)
+		{
+			long ret = 1;
+			for (k %= P - 1; k > 0; k >>= 1, value = value * value % P) {
+				if ((k & 1) == 1) {
+					ret = ret * value % P;
+				}
+			}
+			return new ModInt(ret);
+		}
+
+		public long ToLong()
+		{
+			return value_;
+		}
+
+		public override string ToString()
+		{
+			return value_.ToString();
 		}
 	}
 
@@ -51,7 +182,9 @@ namespace AOJ
 	public static class Helper
 	{
 		public static void UpdateMin<T>(ref T target, T value) where T : IComparable<T>
-			=> target = target.CompareTo(value) > 0 ? value : target;
+		{
+			target = target.CompareTo(value) > 0 ? value : target;
+		}
 
 		public static void UpdateMin<T>(ref T target, T value, Action<T> onUpdated) where T : IComparable<T>
 		{
@@ -62,7 +195,9 @@ namespace AOJ
 		}
 
 		public static void UpdateMax<T>(ref T target, T value) where T : IComparable<T>
-			=> target = target.CompareTo(value) < 0 ? value : target;
+		{
+			target = target.CompareTo(value) < 0 ? value : target;
+		}
 		public static void UpdateMax<T>(ref T target, T value, Action<T> onUpdated) where T : IComparable<T>
 		{
 			if (target.CompareTo(value) < 0) {
@@ -71,10 +206,15 @@ namespace AOJ
 			}
 		}
 
-		public static T[] Array<T>(int n, Func<int, T> init)
-			=> Enumerable.Range(0, n).Select(x => init(x)).ToArray();
+		public static T[] Array<T>(int n, Func<int, T> init) 
+		{
+			return Enumerable.Range(0, n).Select(x => init(x)).ToArray();
+		}
+
 		public static List<T> List<T>(int n, Func<int, T> init)
-			=> Enumerable.Range(0, n).Select(x => init(x)).ToList();
+		{
+			return Enumerable.Range(0, n).Select(x => init(x)).ToList();
+		}
 		public static T[,] Array2<T>(int n, int m, T init)
 		{
 			var array = new T[n, m];
@@ -155,9 +295,19 @@ namespace AOJ
 			return buf_[index_++];
 		}
 
-		public int Int() => int.Parse(Next());
-		public long Long() => long.Parse(Next());
-		public double Double() => double.Parse(Next());
+		public int Int()
+		{
+			return int.Parse(Next());
+		}
+
+		public long Long() {
+			return long.Parse(Next());
+		}
+
+		public double Double()
+		{
+			return double.Parse(Next());
+		}
 
 		public int[] ArrayInt(int N, int add = 0)
 		{
