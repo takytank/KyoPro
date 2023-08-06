5ddc5043d0bc3505459403dd5a1d84e6d9002cd7
takytank <taki_taki_taky@hotmail.co.jp>
2023-08-06 18:37:35 +0900

    MojaCoder の template から Extensions を削除

--- projects/MojaCoder.Temp/ContestPool.cs
+++ projects/MojaCoder.Temp/ContestPool.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using TakyTank.KyoProLib.CSharp;
 using TakyTank.KyoProLib.CSharp.V7;
--- projects/MojaCoder/Program.cs
+++ projects/MojaCoder/Program.cs
@@ -558,20 +558,6 @@ namespace MojaCoder
 			=> string.Join(Environment.NewLine, values);
 	}
 
-	public static class Extensions
-	{
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Span<T> AsSpan<T>(this List<T> list)
-		{
-			return Unsafe.As<FakeList<T>>(list).Array.AsSpan(0, list.Count);
-		}
-
-		private class FakeList<T>
-		{
-			public T[] Array = null;
-		}
-	}
-
 	public class Scanner : IDisposable
 	{
 		private const int BUFFER_SIZE = 1024;
