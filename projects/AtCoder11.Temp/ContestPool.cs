fd06316ae83bac7c61e117ec19d5b1c9d1f8e953
takytank <taki_taki_taky@hotmail.co.jp>
2023-08-06 19:39:03 +0900

    AtCoder11.Temp プロジェクトを追加

---
+++ projects/AtCoder11.Temp/ContestPool.cs
@@ -0,0 +1,91 @@
+﻿using TakyTank.KyoProLib.CSharp.V11;
+
+namespace AtCoder11.Temp;
+
+class ContestPool
+{
+	public static void A()
+	{
+		using var cin = new Scanner();
+	}
+
+	public static void B()
+	{
+		using var cin = new Scanner();
+	}
+
+	public static void C()
+	{
+		using var cin = new Scanner();
+	}
+
+	public static void D()
+	{
+		using var cin = new Scanner();
+	}
+
+	public static void E()
+	{
+		using var cin = new Scanner();
+	}
+
+	public static void F()
+	{
+		using var cin = new Scanner();
+	}
+
+	public static void G()
+	{
+		using var cin = new Scanner();
+	}
+
+	public static void H()
+	{
+		using var cin = new Scanner();
+	}
+
+	public static void I()
+	{
+		using var cin = new Scanner();
+	}
+
+	public static void J()
+	{
+		using var cin = new Scanner();
+	}
+
+	public static void K()
+	{
+		using var cin = new Scanner();
+	}
+
+	public static void L()
+	{
+		using var cin = new Scanner();
+	}
+
+	public static void M()
+	{
+		using var cin = new Scanner();
+	}
+
+	public static void N()
+	{
+		using var cin = new Scanner();
+	}
+
+	public static void O()
+	{
+		using var cin = new Scanner();
+	}
+
+	public static void P()
+	{
+		using var cin = new Scanner();
+	}
+
+	public static void Q()
+	{
+		using var cin = new Scanner();
+	}
+}
