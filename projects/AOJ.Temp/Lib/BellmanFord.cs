d424f39a27b4244983fbb4eed3e464e02d895890
takytank <taki_taki_taky@hotmail.co.jp>
2024-10-30 00:19:55 +0900

    temp の Bellmanford から WarshallFloyd を削除

--- projects/AOJ.Temp/Lib/BellmanFord.cs
+++ projects/AOJ.Temp/Lib/BellmanFord.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AOJ.Temp.Lib
 {
@@ -134,35 +131,5 @@ namespace AOJ.Temp.Lib
 			public int To;
 			public long Cost;
 		}
-	}
-
-	public class WarshallFloyd
-	{
-		/*
-		 * var d = new long[n, n];
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					if (i != j) {
-						d[i, j] = inf;
-					}
-				}
-			}
-
-			for (int i = 0; i < m; i++) {
-				int a = cin.Int() - 1;
-				int b = cin.Int() - 1;
-				long c = cin.Long();
-				d[a, b] = c;
-				d[b, a] = c;
-			}
-
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					for (int k = 0; k < n; k++) {
-						d[j, k] = Math.Min(d[j, k], d[j, i] + d[i, k]);
-					}
-				}
-			}
-			*/
 	}
 }
